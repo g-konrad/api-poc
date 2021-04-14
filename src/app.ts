4f45c348c317439a60b0f629cecb7830739b96b8
Gustavo Konrad <g.konrad@outlook.com>
2021-04-14 17:32:23 -0300

    swagger ainda n funciona

--- src/app.ts
+++ src/app.ts
@@ -8,6 +8,24 @@ import swaggerConfig from './swagger'
 const app: FastifyInstance<Server, IncomingMessage, ServerResponse> = fastify({ logger: true })
 
 export default () => {
+  const opts = {
+    schema: {
+      response: {
+        200: {
+          description: 'Successful response',
+          type: 'object',
+          properties: {
+            hello: { type: 'string' }
+          }
+        }
+      }
+    }
+  }
+
+  app.get('/', opts, (req, res) => {
+    res.send({ hello: 'world' })
+  })
+
   app.register(swagger, swaggerConfig)
 
   app.ready((err) => err != null ? err : app.swagger())
