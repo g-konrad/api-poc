e58489c2558eaf88aa2fd0653b77eea0cc88a51b
Gustavo Konrad <g.konrad@outlook.com>
2021-04-15 16:21:50 -0300

    estrutura mais espelhada

--- fastify/src/app.ts
+++ fastify/src/app.ts
@@ -4,13 +4,13 @@ import type { Server, IncomingMessage, ServerResponse } from 'http'
 import fastify, { FastifyLoggerInstance } from 'fastify'
 import multipart from 'fastify-multipart'
 import swagger from 'fastify-swagger'
-import swaggerConfig from './swagger'
+import { swaggerCfg } from './config'
 import { ping, fileUpload } from './routes'
 
 const createApp = async (): Promise<FastifyInstance<Server, IncomingMessage, ServerResponse, FastifyLoggerInstance>> => {
   const app: FastifyInstance<Server, IncomingMessage, ServerResponse> = fastify({ logger: true })
 
-  await app.register(swagger, swaggerConfig)
+  await app.register(swagger, swaggerCfg)
   await app.register(multipart)
 
   app.route(ping.get())
---
+++ fastify/src/config/index.ts
@@ -0,0 +1,1 @@
+export { default as swaggerCfg } from './swagger'
