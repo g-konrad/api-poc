c7e973ac2f9b465b8f1dd12da53c2cc5adfe219e
Gustavo Konrad <g.konrad@outlook.com>
2021-04-16 10:20:13 -0300

    gin ping

--- gin/main.go
+++ gin/main.go
@@ -1,17 +1,24 @@
 package main
 
 import (
-	"net/http"
+	"github.com/api-poc/gin/routes"
 
 	"github.com/gin-gonic/gin"
+	swaggerFiles "github.com/swaggo/files"
+	ginSwagger "github.com/swaggo/gin-swagger"
+	_ "github.com/swaggo/gin-swagger/example/basic/docs"
 )
 
 func main() {
 	r := gin.Default()
+
 	r.Use(gin.Logger())
 	r.Use(gin.Recovery())
-	r.GET("/ping", func(c *gin.Context) {
-		c.String(http.StatusOK, "pong")
-	})
+
+	url := ginSwagger.URL("http://localhost:8080/swagger/doc.json") // The url pointing to API definition
+	r.GET("/swagger/*any", ginSwagger.WrapHandler(swaggerFiles.Handler, url))
+
+	r.GET("/ping", routes.PingGet)
+
 	r.Run(":3000")
 }
---
+++ gin/routes/ping.go
@@ -0,0 +1,10 @@
+package routes
+
+import (
+	"net/http"
+	"github.com/gin-gonic/gin"
+)
+
+func PingGet (c *gin.Context) {
+	c.String(http.StatusOK, "pong")
+}
