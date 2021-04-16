a15b97a70d0c25ac89356c4a34d4cc8ca40ee13d
Lucas Simoni Mendes <lucas.mendes@yungas.com.br>
2021-04-16 12:27:54 -0300

    implement upload in go lang mvp

--- fastify/src/routes/file-upload/index.ts
+++ fastify/src/routes/file-upload/index.ts
@@ -7,7 +7,7 @@ const TEMPLATE = `
   <html>
       <head><title>Upload Test</title></head>
       <body>
-          <form target="/" method="post" enctype="multipart/form-data">
+          <form target="" method="post" enctype="multipart/form-data">
               <input type="file" multiple name="file"/>
               <button type="submit">Submit</button>
           </form>
